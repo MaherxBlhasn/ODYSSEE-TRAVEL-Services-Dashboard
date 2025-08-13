4840f83b97a5fef123fb47e2735b783f91c04111
maher <mahertun5@gmail.com>
2025-08-13 13:46:50 +0100

    fixing_session_alert

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/lib/AuthContext'
-import SessionAlert from '@/components/SessionAlert'
+// import SessionAlert from '@/components/SessionAlert'
 
 const inter = Inter({ subsets: ['latin'] })
 
