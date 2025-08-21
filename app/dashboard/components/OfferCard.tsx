0a228cd4a6314799f57d1f227e45ec032631249e
maher <mahertun5@gmail.com>
2025-08-21 02:46:06 +0100

    fix

--- app/dashboard/components/OfferCard.tsx
+++ app/dashboard/components/OfferCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { MapPin, Calendar, Star, Trash2, Eye } from 'lucide-react'
+import { MapPin,Star, Trash2, Eye } from 'lucide-react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import ConfirmationModal from '../../../components/ui/ConfirmationModal'
